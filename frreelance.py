4faaaf7102c53ff816650615f35075ef073878d2
Python The Hack <lrulvoni@gmail.com>
2022-07-06 18:01:45 +0200

    update database

--- frreelance.py
+++ frreelance.py
@@ -11,11 +11,39 @@ class Persona:
     def creaPersona(self):
         try:
             print("try")
+            persona = {
+                "nome": self.nome,
+                "cognome": self.cognome,
+                "sesso": self.sesso,
+                "anni": self.anni,
+                "nascita": self.nascita,
+                "indirizzo": self.indirizzo,
+                "email": self.email
+
+            }
+            for key, value in persona.items():
+                print(f'{key}={value}')
         except:
             print("except")
         finally: 
             print("finally")
 
+class Lavoro:
+    def __init__(self, titolo = 'null', desc = 'null', price = 0, location = 'null'):
+        self.titolo = str(titolo)
+        self.desc = str(desc)
+        self.price = int(price)
+        self.location = str(location)  
 
-p1 = Persona()
+    def creaLavoro(self):
+        print('test crea lavoro')
+        
+
+
+
+
+
+p1 = Persona('luca','rulvoni')
 p1.creaPersona() 
+p2 = Lavoro('developer') 
+p2.creaLavoro()
